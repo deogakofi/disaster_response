c0efeb774d313d64f7502ed51ed5628971eb930e
Deoga Kofi <deogakofi@gmail.com>
2020-07-24 10:37:44 +0100

    add runtime.txt for heroku

--- app/run.py
+++ app/run.py
@@ -8,7 +8,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-import joblib
+from sklearn.externals import joblib
 from sqlalchemy import create_engine
 import os
 from flask import Flask
